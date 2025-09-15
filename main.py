a49992d1e70ae209da6097616344655059a312e7
Luis Medina <lmmedina1205@gmail.com>
2025-09-14 19:33:43 -0500

    Agregar modelo de usuario y ruta POST para recibir datos en la API

--- main.py
+++ main.py
@@ -1,10 +1,29 @@
 from fastapi import FastAPI
 import uvicorn
+from pydantic import BaseModel
+from typing import Optional
+from datetime import datetime
+
+#User Model
+class User(BaseModel): #Schema
+    id:int
+    nombre:str
+    apellido:str
+    direccion:Optional[str]
+    telefono:int
+    creacion_user:datetime = datetime.now()
+
 app = FastAPI()
 
 @app.get("/ruta1")
 def ruta1():
     return {"mensaje": "Bienvenido a tu primera api con FastAPI"}
 
+@app.post("/ruta2")
+def ruta2(user: User):
+    usuario = user.dict()
+    print(usuario)
+    return True
+
 if __name__ == "__main__":
-    uvicorn.run("main:app", port=8000)
+    uvicorn.run("main:app", port=8000, reload=True)
